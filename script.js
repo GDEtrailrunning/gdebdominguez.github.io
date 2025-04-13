17373d9d9f6f0dc0904dfa1d291cd87253e51ebf
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-04-13 13:55:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
   // Datos para el gráfico de líneas
 const dataLine1 = {
-  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
     label: 'Km recorridos en Semana 15',
     data: [18, 17, 21, 20, 28], // Datos de los km recorridos en cada día
