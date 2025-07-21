94ed04da1da2bf9789f3290db53e9c2df70102f7
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-07-21 00:19:03 -0300

    Commit weeks 29 & 30

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  //actualizarDiasRestantes();
-  //setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
+  actualizarDiasRestantes();
+  setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
   // Datos para el gráfico de líneas
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 27',
-    data: [15, 19, 23, 14, 24],// Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 29',
+    data: [21, 23, 24, 20, 27],// Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Dí0a 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 28',
-    data: [10, 18, 29, 15, 27], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 30',
+    data: [17, 22, 13, 22, 35], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
